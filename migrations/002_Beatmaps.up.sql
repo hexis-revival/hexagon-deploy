4d429db61a8c9124c0a833afc72c906cb81517aa
Lekuru <contact@lekuru.xyz>
2024-12-10 22:17:33 +0100

    Fix schemas

--- migrations/002_Beatmaps.up.sql
+++ migrations/002_Beatmaps.up.sql
@@ -4,9 +4,9 @@ CREATE TABLE beatmapsets (
     artist character varying(255) NOT NULL,
     source character varying(255) NOT NULL,
     creator_id integer NOT NULL REFERENCES users (id),
-    created_at timestamp without timezone NOT NULL DEFAULT now(),
-    last_updated timestamp without timezone NOT NULL DEFAULT now(),
-    approved_at timestamp without timezone NOT NULL DEFAULT now(),
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    last_updated timestamp without time zone NOT NULL DEFAULT now(),
+    approved_at timestamp without time zone NOT NULL DEFAULT now(),
     approved_by integer NOT NULL REFERENCES users (id),
     status smallint NOT NULL DEFAULT -3,
     description text NOT NULL DEFAULT '',
@@ -22,8 +22,8 @@ CREATE TABLE beatmaps (
     version character varying(255) NOT NULL,
     filename character varying(512) NOT NULL,
     creator_id integer NOT NULL REFERENCES users (id),
-    created_at timestamp without timezone NOT NULL DEFAULT now(),
-    last_updated timestamp without timezone NOT NULL DEFAULT now(),
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    last_updated timestamp without time zone NOT NULL DEFAULT now(),
     status smallint NOT NULL DEFAULT -3,
     total_length integer NOT NULL DEFAULT 0,
     drain_length integer NOT NULL DEFAULT 0,
@@ -39,9 +39,9 @@ CREATE TABLE beatmaps (
     hp real NOT NULL DEFAULT 0,
     od real NOT NULL DEFAULT 0,
     ar real NOT NULL DEFAULT 0,
-    sr real NOT NULL DEFAULT 0,
+    sr real NOT NULL DEFAULT 0
 );
 
 CREATE INDEX idx_beatmaps_id ON beatmaps(id);
 CREATE INDEX idx_beatmaps_set_id ON beatmaps(set_id);
-CREATE INDEX idx_beatmaps_beatmap_md5 ON beatmaps(beatmap_md5);
+CREATE INDEX idx_beatmaps_checksum ON beatmaps(checksum);
