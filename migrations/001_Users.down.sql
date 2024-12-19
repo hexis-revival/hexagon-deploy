32feb9d2ad78ac9057669d96b150f289fa313adf
Lekuru <contact@lekuru.xyz>
2024-12-19 22:28:40 +0100

    Fix down migration

--- migrations/001_Users.down.sql
+++ migrations/001_Users.down.sql
@@ -5,10 +5,9 @@ DROP INDEX idx_stats_user_id;
 DROP INDEX idx_users_name;
 DROP INDEX idx_users_id;
 
-DROP TYPE relationship_status;
-
 DELETE FROM relationships;
 DROP TABLE relationships;
+DROP TYPE relationship_status;
 
 DELETE FROM stats;
 DROP TABLE stats;
