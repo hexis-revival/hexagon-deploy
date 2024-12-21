8e1991539aee149e5f1a03e203a9779d02020df9
Lekuru <contact@lekuru.xyz>
2024-12-22 00:54:50 +0100

    Set default user to activated by default

--- migrations/007_DefaultUsers.up.sql
+++ migrations/007_DefaultUsers.up.sql
@@ -1,2 +1,3 @@
-INSERT INTO users (name, email, password, country) VALUES ('Dienay', 'contact@dienay.net', '$2a$10$uUvzbCIBcFLdxDK6QcGZ3O.K0/4Z3ObFsy0BvetogYdE3IXdn7Q4y', 'CA');
+INSERT INTO users (name, email, password, country, activated)
+  VALUES ('Dienay', 'contact@dienay.net', '$2a$10$uUvzbCIBcFLdxDK6QcGZ3O.K0/4Z3ObFsy0BvetogYdE3IXdn7Q4y', 'CA', true);
 ALTER SEQUENCE users_id_seq RESTART WITH 100;
